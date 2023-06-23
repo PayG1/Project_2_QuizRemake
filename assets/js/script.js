3d747969141ce888d7d1c55f919dc23459d1b556
PayG1 <micah.athe.009@gmail.com>
2023-06-23 08:54:29 +0000

    uploaded files locally to gitpod

---
+++ assets/js/script.js
@@ -0,0 +1,450 @@
+
+let questions = [
+    {
+        question: "Which animal is known for its ability to mimic human speech?", 
+        answers: [
+            {text: "Parrot", correct: true},
+            {text: "Raven", correct: false},
+            {text: "Hummingbird", correct: false},
+            {text: "Owl", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal is known for its exceptional memory?", 
+        answers: [
+            {text: "Chimpanzee", correct: false},
+            {text: "Dolphin", correct: false},
+            {text: "Octopus", correct: false},
+            {text: "Elephant", correct: true} 
+        ]
+    },
+    {
+        question: "Which animal is venomous and has a hood?",
+        answers: [
+            {text: "Cobra", correct: true},
+            {text: "Rattlesnake", correct: false},
+            {text: "Boa Constrictor", correct: false},
+            {text: "Python", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal has the largest number of legs?",
+        answers: [
+            {text: "Centipede", correct: false},
+            {text: "Millipede", correct: true},
+            {text: "Scorpion", correct: false},
+            {text: "Spider", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal is known for its ability to use tools?",
+        answers: [
+            {text: "Gorilla", correct: false},
+            {text: "Koala", correct: false},
+            {text: "Orangutan", correct: true},
+            {text: "Sloth", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal has the largest eyes relative to its body size?",
+        answers: [
+            {text: "Giraffe", correct: false},
+            {text: "Giant Squid", correct: true},
+            {text: "Kangaroo", correct: false},
+            {text: "Ostrich", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal is known for its ability to regenerate lost body parts?",
+        answers: [
+            {text: "Sea Urchin", correct: false},
+            {text: "Sea Cucumber", correct: false},
+            {text: "Jellyfish", correct: false},
+            {text: "Starfish", correct: true} 
+        ]
+    },
+    {
+        question: "Which animal is known for its nocturnal lifestyle and exceptional hearing?",
+        answers: [
+            {text: "Bats", correct: false},
+            {text: "Bushbaby", correct: false},
+            {text: "Barn Owl", correct: true},
+            {text: "Tarsier", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal is capable of producing electricity?",
+        answers: [
+            {text: "Stingray", correct: false},
+            {text: "Blobfish", correct: false},
+            {text: "Anglerfish", correct: false},
+            {text: "Electric Eel", correct: true} 
+        ]
+    },
+    {
+        question: "Which animal has the largest brain-to-body ratio among all land animals?",
+        answers: [
+            {text: "Dolphin", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Human", correct: false},
+            {text: "Whale", correct: false} 
+        ]
+    }   
+];
+
+let  question_set2 = [
+    {
+        question: "Which animal is known for its ability to camouflage itself in its environment?",
+        answers: [
+            {text: "Chameleon", correct: false},
+            {text: "Octopus", correct: true},
+            {text: "Cuttlefish", correct: false},
+            {text: "Mimic Octopus", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal is the largest species of penguin?",
+        answers: [
+            {text: "Emperor Penguin", correct: true},
+            {text: "King Penguin", correct: false},
+            {text: "Gentoo Penguin", correct: false},
+            {text: "Adélie Penguin", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal has the longest migration route?",
+        answers: [
+            {text: "Arctic Tern", correct: true},
+            {text: "Monarch Butterfly", correct: false},
+            {text: "Humpback Whale", correct: false},
+            {text: "Wildebeest", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal has the largest number of teeth?",
+        answers: [
+            {text: "Giant Otter", correct: false},
+            {text: "Giant Anteater", correct: false},
+            {text: "Giant Panda", correct: false},
+            {text: "Giant Armadillo", correct: true} 
+        ]
+    },
+    {
+        question: "Which animal is known for its ability to regrow its tail?",
+        answers: [
+            {text: "Anole", correct: false},
+            {text: "Salamander", correct: false},
+            {text: "Gecko", correct: false},
+            {text: "Lizard", correct: true} 
+        ]
+    },
+    {
+        question: "Which animal has the ability to walk on water?",
+        answers: [
+            {text: "Flying Fish", correct: false},
+            {text: "Jesus Christ Lizard", correct: true},
+            {text: "Basalisk Lizard", correct: false},
+            {text: "Water Strider", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal has the longest lifespan in captivity?",
+        answers: [
+            {text: "African Elephant", correct: false},
+            {text: "Bowhead Whale", correct: false},
+            {text: "Galapagos Tortoise", correct: true},
+            {text: "Macaw", correct: false}
+            
+        ]
+    },
+    {
+        question: "Which animal is known for its exceptional problem-solving skills?",
+        answers: [
+            {text: "Crow", correct: true},
+            {text: "Honey Badger", correct: false},
+            {text: "Raccoon", correct: false},
+            {text: "Squirrel", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal is the only mammal capable of sustained flight?",
+        answers: [
+            {text: "Flying Lemur", correct: false},
+            {text: "Flying Squirrel", correct: false},
+            {text: "Sugar Glider", correct: false},
+            {text: "Bat", correct: true} 
+        ]
+    },
+    {
+        question: "Which animal has the longest tongue relative to its body size?",
+        answers: [
+            {text: "Chameleon", correct: true},
+            {text: "Giraffe", correct: false},
+            {text: "Lizard", correct: false},
+            {text: "Anteater", correct: false} 
+        ]
+    }   
+];
+
+let  question_set3 = [
+    {
+        question: "Which animal is known for its ability to echolocate?",
+        answers: [
+            {text: "Dolphin", correct: false},
+            {text: "Bat", correct: true},
+            {text: "Giraffe", correct: false},
+            {text: "Elephant", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal is capable of producing the loudest sound in the animal kingdom?",
+        answers: [
+            {text: "Lion", correct: false},
+            {text: "Elephant", correct: false},
+            {text: "Howler Monkey", correct: false},
+            {text: "Blue Whale", correct: true} 
+        ]
+    },
+    {
+        question:  "Which animal is known for its ability to glide through the air?",
+        answers: [
+            {text: "Flying Squirrel", correct: true},
+            {text: "Kangaroo", correct: false},
+            {text: "Armadillo", correct: false},
+            {text: "Hedgehog", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal is known for its excellent eyesight and ability to spot prey from long distances?",
+        answers: [
+            {text: "Owl", correct: false},
+            {text: "Eagle", correct: true},
+            {text: "Hawk", correct: false},
+            {text: "Falcon", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal is capable of producing venom but is not a snake or spider?",
+        answers: [
+            {text: "Komodo Dragon", correct: false},
+            {text: "Scorpionfish", correct: false},
+            {text: "Jellyfish", correct: false},
+            {text: "Platypus", correct: true} 
+        ]
+    },
+    {
+        question: "Which animal is known for its ability to hibernate during the winter months?",
+        answers: [
+            {text: "Bear", correct: true},
+            {text: "Wolf", correct: false},
+            {text: "Fox", correct: false},
+            {text: "Deer", correct: false} 
+        ]
+    },
+    {
+        question: "Which animal is known for its incredible jumping ability?",
+        answers: [
+            {text: "Grasshopper", correct: false},
+            {text: "Gazelle", correct: false},
+            {text: "Kangaroo", correct: false},
+            {text: "Flea", correct: true} 
+        ]
+    },
+    {
+        question: "Which animal has the highest body temperature of any mammal?",
+        answers: [
+            {text: "Thermometer", correct: false},
+            {text: "Dromedary Camel", correct: false},
+            {text: "Polar Bear", correct: false},
+            {text: "Arctic Fox", correct: true} 
+        ]
+    },
+    {
+        question: "Which animal is known for its ability to produce bioluminescent light?", 
+        answers: [
+            {text: "Anglerfish", correct: false},
+            {text: "Glowworm", correct: false},
+            {text: "Firefly", correct: true},
+            {text: "Jellyfish", correct: false}
+            
+        ]
+    },
+    {
+        question: "Which animal is known for its highly developed social structure and complex communication?", 
+        answers: [
+            {text: "Lion", correct: false},
+            {text: "Gorilla", correct: false},
+            {text: "Elephant", correct: false},
+            {text: "Orca", correct: true} 
+        ]
+    }
+];
+
+//Global variables
+const userMessage = document.getElementById("user-message");
+const questionText = document.getElementById('question-text');
+const startOver = document.getElementById('start-over'); 
+let answers = document.getElementById('answers');
+
+let answerStatus;
+let rightButton;
+let rightAnswer;
+
+
+let questionIndex = 0;
+let capitalQuestionIndex = 0;
+let question_set2Index = 0;
+let question_set3Index = 0; 
+let wrongAnswers = 0;
+
+questions = shuffle(questions);
+question_set2 = shuffle(question_set2);
+question_set3= shuffle(question_set3);
+
+const beginGameBtn = document.getElementById('next-btn');
+beginGameBtn.addEventListener('click', runQuiz);
+/**
+ * Shuffles the questions.
+ */
+//This comes from codebubb, https://www.youtube.com/watch?v=5sNGqsMpW1E
+//It's the Fisher Yates algorithm. 
+function shuffle(questions) {
+    for (let i = questions.length-1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i+1));
+      const temp = questions[i];
+      questions[i] = questions[j];
+      questions[j] = temp;
+    }
+    return questions;
+  }
+
+ function runQuiz(){
+    userMessage.classList.add("hide");
+    beginGameBtn.classList.add('hide');
+    startOver.classList.add('hide');
+    //insert questions and answers
+    beginGameBtn.innerText = "Next Question";
+    answers.classList.remove('hide');
+    addQuestion();
+    questionIndex++;
+}
+
+/**
+ * Adds a question to game after deciding which set to use. Calls functions
+ * to shuffle the questions and to insert the answers.
+ */
+ function addQuestion() {
+    answers.innerHTML = "";
+    let theme = document.getElementById('theme');
+    //Decide which set of questions to choose
+    if (questionIndex < 5) {
+        theme.innerHTML = "Theme: Animals";
+        questionText.innerHTML = questions[capitalQuestionIndex].question;
+        insertAnswers();
+        capitalQuestionIndex++;
+    } else if (questionIndex >= 5 && questionIndex <10) {
+        theme.innerText = "Theme: Animals";
+        questionText.innerHTML = question_set2[question_set2Index].question;
+        insertAnswers();
+        question_set2Index++;
+    } else if (questionIndex >= 10 && questionIndex <15) {
+        theme.innerText = "Theme: Animals";
+        questionText.innerHTML = question_set3[question_set3Index].question;
+        insertAnswers();
+        question_set3Index++;
+    } else {
+        gameOver();
+    }
+}
+
+ function insertAnswers() {
+    let btnText;
+    userMessage.classList.add('hide');
+    for (let i=0; i <4; i++) {
+        let newBtn = document.createElement('button');
+        //Use the index to find the correct question and determine if answers are true or false
+        if (capitalQuestionIndex <5) {
+            btnText = questions[capitalQuestionIndex].answers[i].text;
+            answerStatus = questions[capitalQuestionIndex].answers[i].correct;
+        } else if (capitalQuestionIndex >= 5 && question_set2Index < 5){
+            btnText = question_set2[question_set2Index].answers[i].text;
+            answerStatus = question_set2[question_set2Index].answers[i].correct;
+        } else if (question_set2Index >= 5 &&  question_set2Index < 5) {
+            btnText = question_set3[question_set2Index].answers[i].text;
+            answerStatus = question_set3[question_set2Index].answers[i].correct;
+        }
+        newBtn.innerText = btnText;
+        newBtn.classList.add('btn');
+        newBtn.disabled = false;
+        newBtn.dataset.correct = answerStatus;
+        if (answerStatus === true) {
+            rightAnswer = btnText;
+            rightButton = newBtn;
+        }
+        newBtn.addEventListener('click', findUserAnswer);
+        answers.appendChild(newBtn);
+    }
+}
+
+
+ function findUserAnswer(e) {
+    let win;
+
+    let userButton = e.target;
+    let userAnswer = e.target.innerText;
+    let allButtons = document.querySelectorAll('.btn');
+    for (let i =0; i<4; i++) {
+        let currentButton = allButtons[i];
+        currentButton.disabled = true;
+        currentButton.classList.remove('btn');
+        currentButton.classList.add('btn-no-hover');
+    }  
+    if (userAnswer != rightAnswer){
+        win = false;
+        wrongAnswers++;
+    } else if (userAnswer === rightAnswer) {
+        win = true;
+    }
+    if (win === true) {
+        userButton.style.background = "green";
+        let oldScore = parseInt(document.getElementById("score").innerText);
+        document.getElementById("score").innerText = ++oldScore;
+    } else if (win != true) {
+        userButton.style.background = "red";
+        rightButton.style.background = "green";
+        Life_lost();
+    }
+    if (wrongAnswers != 3) {
+        beginGameBtn.classList.remove('hide');
+    }
+ }
+ 
+function Life_lost() {
+    if (wrongAnswers === 1) {
+        document.getElementById('pint3').src = "assets/images/x-icon.png";
+        document.getElementById('pint3').alt = "X icon replesenting a incorrect answer.";
+    }
+    if (wrongAnswers === 2) {
+        document.getElementById('pint2').src = "assets/images/x-icon.png";
+        document.getElementById('pint2').alt = "X icon replesenting a incorrect answer.";
+    }
+    if (wrongAnswers === 3) {
+        document.getElementById('pint1').src = "assets/images/x-icon.png";
+        document.getElementById('pint1').alt = "X icon replesenting a incorrect answer.";
+        setTimeout(gameOver, 1500);
+    }
+}
+ function gameOver() {
+    answers.innerHTML = "";
+    if (wrongAnswers === 0) {
+        questionText.innerHTML = "Well done, you are so smart!";
+    } else if (wrongAnswers === 1) {
+        questionText.innerHTML = "That was close, aim for ta perfect score.";
+    } else if (wrongAnswers === 2) {
+        questionText.innerHTML = "You are on your way there, keep trying.";
+    } else {
+        questionText.innerHTML = "Everyone makes mistakes, keep trying.";
+    }
+    startOver.classList.remove('hide');
+    beginGameBtn.classList.add('hide'); 
+}
